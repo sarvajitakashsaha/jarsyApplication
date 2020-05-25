f648a6701a82d49664b556597e14b5f182c99a52
sarvajit saha <saha.sarvajit@gmail.com>
2020-05-25 22:20:46 +0530

    adding logging file

--- currency-conversion-service/src/main/java/com/example/springboot/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/springboot/CurrencyConversionController.java
@@ -1,13 +1,20 @@
 package com.example.springboot;
 
 import java.math.BigDecimal;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,9 +29,9 @@ public class CurrencyConversionController {
 	@Autowired
 	CurrencyExchangeServiceProxy proxy;
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}/quantity/{quantity}")
-
-	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
+//using RestTemplate getForEntity() 
+	@GetMapping("/currency-exchange/from/{fr}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrency(@PathVariable("fr") String from, @PathVariable String to,
 			@PathVariable BigDecimal quantity) {
 		Map<String, String> uriVariables = new HashMap<>();
 		uriVariables.put("from", from);
@@ -32,6 +39,28 @@ public class CurrencyConversionController {
 		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().getForEntity(
 				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
 				uriVariables);
+		System.out.println(" ResponseEntity status code is for getForEntity >>>" + responseEntity.getHeaders());
+		CurrencyConversionBean response = responseEntity.getBody();
+		System.out.println("RESPONSE  is >>>" + response);
+		logger.info("{}", response);
+		return new CurrencyConversionBean(response.getId(), from, to, response.getConvirsionMultiple(), quantity,
+				quantity.multiply(response.getConvirsionMultiple()), 0);
+	}
+
+	// using RestTemplate exchange()
+	@GetMapping("/currency-exchange1/from/{fr}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrency1(@PathVariable("fr") String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+		HttpHeaders headers = new HttpHeaders();
+		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+		HttpEntity<String> entity = new HttpEntity<String>(headers);
+		ResponseEntity<CurrencyConversionBean> responseEntity = new RestTemplate().exchange(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", HttpMethod.GET, entity,
+				CurrencyConversionBean.class, uriVariables);
+		logger.debug(" ResponseEntity status code is for exchange {}", responseEntity.getHeaders());
 		CurrencyConversionBean response = responseEntity.getBody();
 		System.out.println("RESPONSE  is >>>" + response);
 		logger.info("{}", response);
@@ -40,28 +69,30 @@ public class CurrencyConversionController {
 	}
 
 	@GetMapping("/currency-exchange-feign/from/{from}/to/{to}/quantity/{quantity}")
-	@HystrixCommand(fallbackMethod = "getFallBackValue")
-	public ResponseEntity<CurrencyConversionBean> convertCurrencyFeign(@PathVariable String from, @PathVariable String to,
-			@PathVariable BigDecimal quantity) {
+//	@HystrixCommand(fallbackMethod = "getFallBackValue")
+	public ResponseEntity<CurrencyConversionBean> convertCurrencyFeign(@PathVariable String from,
+			@PathVariable String to, @PathVariable BigDecimal quantity) {
 		CurrencyConversionBean response = new CurrencyConversionBean();
 		try {
 			response = proxy.retriveExchangeValue(from, to);
 		} catch (Exception e) {
 			throw new DataNotFoundException("data not present");
 		}
-		System.out.println("RESPONSE  is >>>" + response);
+		// Object session = request;
+		// System.out.println("RESPONSE is >>>" + request.getSession());
 		logger.info("{}", response);
-		CurrencyConversionBean requiredResponse = new CurrencyConversionBean(response.getId(), from, to, response.getConvirsionMultiple(), quantity,
-				quantity.multiply(response.getConvirsionMultiple()), response.getPort());
+		CurrencyConversionBean requiredResponse = new CurrencyConversionBean(response.getId(), from, to,
+				response.getConvirsionMultiple(), quantity, quantity.multiply(response.getConvirsionMultiple()),
+				response.getPort());
 
 		return new ResponseEntity<CurrencyConversionBean>(requiredResponse, HttpStatus.OK);
-				}
+	}
 
 //This Method will be called when CurrencyExchange Service is down
-	public ResponseEntity<CurrencyConversionBean> getFallBackValue(@PathVariable String from, @PathVariable String to,
-			@PathVariable BigDecimal quantity) {
-		CurrencyConversionBean fallBackObj = new CurrencyConversionBean();
-		fallBackObj.setFrom(from);
-		return new ResponseEntity<CurrencyConversionBean>(fallBackObj, HttpStatus.GATEWAY_TIMEOUT);
-	}
+//	public ResponseEntity<CurrencyConversionBean> getFallBackValue(@PathVariable String from, @PathVariable String to,
+//			@PathVariable BigDecimal quantity, final HttpServletRequest request) {
+//		CurrencyConversionBean fallBackObj = new CurrencyConversionBean();
+//		fallBackObj.setFrom(from);
+//		return new ResponseEntity<CurrencyConversionBean>(fallBackObj, HttpStatus.GATEWAY_TIMEOUT);
+//	}
 }
--- currency-conversion-service/src/main/java/com/example/springboot/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/example/springboot/CurrencyConversionServiceApplication.java
@@ -8,11 +8,16 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import brave.sampler.Sampler;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableFeignClients("com.example.springboot")
 @EnableEurekaClient
 @EnableHystrix
+@EnableSwagger2
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
@@ -22,5 +27,10 @@ public class CurrencyConversionServiceApplication {
 	public Sampler defaultSampler(){
 		return Sampler.ALWAYS_SAMPLE;
 	}
+	@Bean
+	   public Docket productApi() {
+	      return new Docket(DocumentationType.SWAGGER_2).select()
+	         .apis(RequestHandlerSelectors.basePackage("com.example.springboot")).build();
+	   }
 
 }
